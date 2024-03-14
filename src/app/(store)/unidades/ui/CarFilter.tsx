30f68dc2483ca6c6dcd972ba237a27984e08181c
Kevin Defalco <kevindefalco@gmail.com>
2024-03-14 00:56:41 -0300

    rendering carfilter with query params next-navigation

--- src/app/(store)/unidades/ui/CarFilter.tsx
+++ src/app/(store)/unidades/ui/CarFilter.tsx
@@ -1,18 +1,23 @@
 'use client'
 
 import { RedirectAnimation } from "@/components";
-import { UseRedirect } from "@/hooks/useRedirect";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { stringify } from "querystring";
-import { Suspense } from "react";
+import { Suspense, use, useEffect, useState } from "react";
 
 export const CarFilter = () => {
 
-  const { redirectTo, isRedirecting } = UseRedirect();
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const [isRedirecting, setIsRedirecting] = useState(false);
+  const url = pathname + '?' + searchParams.toString();
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleChange = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setIsRedirecting(true);
     const value = event.target.value;
     let orderBy, orderDirection;
-
+    
     switch (value) {
       case 'menor_precio':
         orderBy = 'price';
@@ -35,11 +40,16 @@ export const CarFilter = () => {
         orderDirection = 'asc';
     }
 
-    redirectTo(`/unidades?${stringify({ orderBy, orderDirection })}`);
+    router.push(`/unidades?${stringify({ orderBy, orderDirection })}`);
 
   };
+
+  useEffect(() => {
+    setIsRedirecting(false);
+  }, [url, setIsRedirecting]);
+ 
+
   return (
-    <>
     <Suspense>
       <div className="relative">
         <select onChange={handleChange} className="block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-2 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
@@ -57,6 +67,5 @@ export const CarFilter = () => {
       </div>
       <RedirectAnimation isRedirecting={isRedirecting} />
     </Suspense>
-    </>
   )
 }
