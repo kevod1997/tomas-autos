d7ce27f5b2b0043a28ba35421b4bdb25d385dea9
Kevin Defalco <kevindefalco@gmail.com>
2024-03-14 16:57:44 -0300

    v2

--- src/components/ui/skeleton/Skeleton.tsx
+++ src/components/ui/skeleton/Skeleton.tsx
@@ -1,9 +1,8 @@
+import './skeleton.css';
+
 export const Skeleton = () => {
     return (
-        <div className="relative w-full h-full rounded-lg overflow-hidden">
-        <div className="animate-pulse absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200"></div>
-        <div className="absolute inset-0 bg-gradient-to-r from-transparent to-gray-200 via-gray-300  animate-shine"></div>
-      </div>
+      <div className="w-full h-full bg-gray-300 rounded-lg animate-custom-pulse"></div>
     );
   };
   
