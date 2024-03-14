2a8e950d2ec8d722786644df698ec26e0bbde044
Kevin Defalco <kevindefalco@gmail.com>
2024-03-14 17:04:14 -0300

    v3

--- src/components/ui/skeleton/Skeleton.tsx
+++ src/components/ui/skeleton/Skeleton.tsx
@@ -2,7 +2,7 @@ import './skeleton.css';
 
 export const Skeleton = () => {
     return (
-      <div className="w-full h-full bg-gray-300 rounded-lg animate-custom-pulse"></div>
+      <div className="width-[100%] sm:h-[75vh] ml-auto mr-auto bg-gray-300 rounded-lg animate-custom-pulse"></div>
     );
   };
   
