9dee1614d73ed884f8ff651e6a7a45345aa102b3
Kevin Defalco <kevindefalco@gmail.com>
2024-03-14 01:04:24 -0300

    v4

--- src/app/(store)/unidades/page.tsx
+++ src/app/(store)/unidades/page.tsx
@@ -3,6 +3,7 @@ import { getPaginatedCarsWithImages } from "@/actions";
 import { CarFilter } from "./ui/CarFilter";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
+import { CarFIlterWithSuspense } from "./ui/CarFilterWithSuspense";
 
 export const metadata = {
   title: 'Unidades',
@@ -36,7 +37,7 @@ export default async function CarsPage({ searchParams }: Props) {
           subtitle="Unidades disponibles"
           className="p-2 mb-4 "
         />
-
+        <CarFIlterWithSuspense />
       </div>
       <CarGrid cars={cars!} />
             {totalPages! > 1 && (
